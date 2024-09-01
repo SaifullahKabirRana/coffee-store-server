8a673af4464ba4eefe98ffe07996518defb2ee42
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2024-09-02 02:48:56 +0600

    users api added

--- index.js
+++ index.js
@@ -90,9 +90,21 @@ async function run() {
       res.send(result);
     })
 
+    app.patch('/user', async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const updatedDoc = {
+        $set: {
+          lastLoggedAt: user.lastLoggedAt
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
+
     app.delete('/user/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await userCollection.deleteOne(query);
       res.send(result);
     })
